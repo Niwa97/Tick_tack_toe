54a080372e3f8d6dacec82e70ecd5399c740e173
Niwa97 <48857497+Niwa97@users.noreply.github.com>
2023-11-27 02:07:49 +0100

    Update game.sh

--- game.sh
+++ game.sh
@@ -17,6 +17,8 @@ echo "0 1 2"
 echo "3 4 5"
 echo "6 7 8"
 echo "====================="
+echo "If you want to save game - press S"
+echo "If you want to resume game - press R"
 }
 
 draw_board ()
@@ -73,6 +75,47 @@ then
 fi
 }
 
+save_game ()
+{
+  FILE=saved.txt
+  if test -f "$FILE"; 
+  then
+    rm saved.txt
+  fi
+  touch saved.txt
+  printf "$turn\n" >> saved.txt
+  printf "${board[0]}\n" >> saved.txt
+  printf "${board[1]}\n" >> saved.txt
+  printf "${board[2]}\n" >> saved.txt
+  printf "${board[3]}\n" >> saved.txt
+  printf "${board[4]}\n" >> saved.txt
+  printf "${board[5]}\n" >> saved.txt
+  printf "${board[6]}\n" >> saved.txt
+  printf "${board[7]}\n" >> saved.txt
+  printf "${board[8]}" >> saved.txt
+}
+
+resume_game ()
+{
+  echo "resume"
+  FILE=saved.txt
+  if test -f "$FILE"; 
+  then
+    turn=$(sed '1q;d' saved.txt)
+    board[0]=$(sed '2q;d' saved.txt)
+    board[1]=$(sed '3q;d' saved.txt)
+    board[2]=$(sed '4q;d' saved.txt)
+    board[3]=$(sed '5q;d' saved.txt)
+    board[4]=$(sed '6q;d' saved.txt)
+    board[5]=$(sed '7q;d' saved.txt)
+    board[6]=$(sed '8q;d' saved.txt)
+    board[7]=$(sed '9q;d' saved.txt)
+    board[8]=$(sed '10q;d' saved.txt)
+  else
+    echo "No saved games"
+  fi
+}
+
 player_turn ()
 {
 if [[ $(($turn % 2)) == 1 ]]
@@ -105,6 +148,14 @@ then
       player_turn
     fi
   fi
+elif [[ "$pos" == "S" ]]
+then
+  save_game
+elif [[ "$pos" == "R" ]]
+then
+  resume_game
+  draw_board
+  player_turn
 else
   echo "Choose right position"
   player_turn
